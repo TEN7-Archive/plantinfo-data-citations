0d1a5a10bb64fd83b72d2c53b57a1c0e6f009e76
Heather Brooke <heather.brooke@rainbowarray.com>
2022-05-04 19:17:28 -0500

    Add consolidated journal files.

--- combine.js
+++ combine.js
@@ -1,14 +1,65 @@
 const { combine } = require('combine-json');
-const fs = require('fs');
-const fileName = 'journal_citations/journals/a/advances_in_strawberry_production.json';
+const fs = require('graceful-fs');
+const journalDir = 'journal_citations/journals';
+const currentDirName = 'journal_citations/journals/a/advances_in_strawberry_production';
+const currentFileName = currentDirName + '.json';
 const minified = false;
+const largeDirs =
+  [
+  ];
 
-const combinedJSON = combine('journal_citations/journals/a/advances_in_strawberry_production');
+async function getCombinedJSON(jsonDir) {
+  return await combine(jsonDir);
+}
 
-fs.appendFile(fileName, JSON.stringify(combinedJSON, null, minified ? 0 : 2), function (err) {
-  if (err) {
-    throw err
+async function combineDirFiles(dirName, fileName) {
+  const combinedJSON = await getCombinedJSON(dirName);
+
+  await fs.appendFile(fileName, JSON.stringify(combinedJSON, null, minified ? 0 : 2), function (err) {
+    if (err) {
+      throw err
+    }
+    console.log(`Saved to ${fileName}`)
+  });
+}
+
+async function checkSubDirItem(subDirItem, subDir) {
+  if (largeDirs.includes(subDirItem)) {
+    console.log('excluding ' + subDirItem);
+  } else {
+    const subDirItemPath  = subDir + '/' + subDirItem;
+
+    if (!subDirItem.includes('.json')) {
+      const currentDirName = subDirItemPath;
+      const currentFileName = currentDirName + '.json';
+
+      console.log('combining files for ' + currentDirName);
+      console.log('outputting to ' + currentFileName);
+      await combineDirFiles(currentDirName, currentFileName);
+    }
   }
-  console.log(`Saved to ${fileName}`)
-})
+}
 
+
+function checkSubDir(subDir, dir) {
+  const subDirPath = dir + '/' + subDir;
+  const subDirItems = fs.readdirSync(subDirPath);
+
+  subDirItems.forEach(subDirItem => {
+   checkSubDirItem(subDirItem, subDirPath);
+  });
+}
+
+function checkDirectories() {
+  const journalSubDirectories = fs.readdirSync(journalDir);
+
+  journalSubDirectories.forEach(subDirectory => {
+    checkSubDir(subDirectory, journalDir);
+  });
+}
+
+const subDirectory = 'z';
+
+checkSubDir(subDirectory, journalDir);
+
+// checkDirectories();
